46e9f1742fa140568c33662aaf3771788da42a01
Aurélien Debord <adebord@agencedebord.com>
2022-03-03 19:07:53 +0100

    Release script

---
+++ index.js
@@ -0,0 +1,64 @@
+const airtable = require("airtable");
+const sgMail = require("@sendgrid/mail");
+
+const base = airtable.base(`${process.env.AIRTABLE_BASE}`);
+
+const questionsBase = base("Questions");
+const allQuestions = questionsBase.select({ view: "Grid view" });
+const updateRecord = (record) => {
+  base("Questions").update([
+    {
+      id: record.id,
+      fields: {
+        Status: "Envoyé",
+      },
+    },
+  ]);
+};
+
+const sendMail = (record) => {
+  let fields = record.fields;
+  let text = `
+Bonjour,
+
+Voici notre question pour nos participant.e.s du jour:
+
+${fields.Question}
+
+Réponse : ${fields.Reponse}
+
+Amicales Saluations,
+
+Aurélien Debord
+25 rue du champ de foire
+87130 - La Croisille-sur-Briance
+`;
+
+  let msg = {
+    to: `marie.m.debord@gmail.com`,
+    from: `${process.env.SENDER}`,
+    subject: `Nouvelle question - ${fields.Sujet}`,
+    text: text,
+  };
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  sgMail
+    .send(msg)
+    .then((response) => {
+      if (response[0].statusCode === 202) {
+        updateRecord(record);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+allQuestions.firstPage((error, records) => {
+  let questions = records.map((record) => record._rawJson);
+  let questionsFilter = questions.filter(
+    (question) => question.fields.Status === "A envoyer"
+  );
+
+  sendMail(questionsFilter[0]);
+});
